f3c56152dfda7e1ca31fcf4b24bb9174d89a32f7
Thishan Paramanayagam <jelamable@gmail.com>
2023-05-20 09:34:27 +0530

    Table Created

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.css']
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
 
+  employees: Employee[] = [
+    {
+      id: 'hello1',
+      name : 'Kim',
+      email: 'kim@gmail.com',
+      salary : 10000 ,
+      department : 'IT'
+    },
+    {
+      id: 'hello2',
+      name : 'Kim2',
+      email: 'kim2@gmail.com',
+      salary : 20000 ,
+      department : 'IT'
+    },
+    {
+      id: 'hello3',
+      name : 'Kim3',
+      email: 'kim3@gmail.com',
+      salary : 30000 ,
+      department : 'IS'
+    },
+  ];
+  constructor(){}
+  ngOnInit(): void {
+    
+
+  }
 }
